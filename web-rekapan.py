937b2c25e8bb864ef6b30e2cbc4d74ece616b416
rsiahikmahsawi <143309643+rsiahikmahsawi@users.noreply.github.com>
2023-08-27 12:42:26 +0700

    Add files via upload

---
+++ web-rekapan.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Rekapan Pengunjung RSIA HIMAH SAWI")
+# tanggal = st.date_input("When's your birthday")
+# st.text(tanggal)
+st.markdown('---')
+st.markdown("Pilih File Yang ingin Anda Import")
+uploaded_file = st.file_uploader(
+    " ", type="csv")
+
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df)
+    tanggal = st.text_input(
+        "Masukkan Tanggal Yang Ingin dicari (Tahun-Bulan-Tangaal)")
+    tombol = st.button("Cari Data")
+    if tombol:
+        df_new = df[df['Pilih Tanggal Rencana Kunjungan '] == tanggal]
+        df_new
+        download = st.button("Download")
