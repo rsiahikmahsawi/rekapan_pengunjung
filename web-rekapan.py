83bab917617d6a0a6271be4f361cdecbb07dc89c
rsiahikmahsawi <143309643+rsiahikmahsawi@users.noreply.github.com>
2023-08-28 14:38:10 +0700

    web-rekapan.py

--- web-rekapan.py
+++ web-rekapan.py
@@ -14,6 +14,7 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
     df["Nomer Telepon"] = df["Nomer Telepon"].apply(lambda x: str(x))
     df["Masukkan Nomer Rekam Medis Jika Peserta Lama"] = df["Masukkan Nomer Rekam Medis Jika Peserta Lama"].apply(lambda x: str(x))
+    df['Masukkan Nomer Rekam Medis Jika Peserta Lama'] = df['Masukkan Nomer Rekam Medis Jika Peserta Lama'].astype(str).str.replace('.0', '')
     df = st.dataframe(df)
     tanggal = st.text_input(
         "Masukkan Tanggal Yang Ingin dicari (Tahun-Bulan-Tangaal)")
