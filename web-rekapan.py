9b897596c09ccafff61dbd8e3c9c1428bd30af62
rsiahikmahsawi <143309643+rsiahikmahsawi@users.noreply.github.com>
2023-08-28 14:43:14 +0700

    web-rekapan.py

--- web-rekapan.py
+++ web-rekapan.py
@@ -15,11 +15,11 @@ if uploaded_file:
     df["Nomer Telepon"] = df["Nomer Telepon"].apply(lambda x: str(x))
     df["Masukkan Nomer Rekam Medis Jika Peserta Lama"] = df["Masukkan Nomer Rekam Medis Jika Peserta Lama"].apply(lambda x: str(x))
     df['Masukkan Nomer Rekam Medis Jika Peserta Lama'] = df['Masukkan Nomer Rekam Medis Jika Peserta Lama'].astype(str).str.replace('.0', '')
-    df = st.dataframe(df)
-    tanggal = st.text_input(
+    df
+    tanggal=st.text_input(
         "Masukkan Tanggal Yang Ingin dicari (Tahun-Bulan-Tangaal)")
     tombol = st.button("Cari Data")
     if tombol:
-        df_new = df[df['Pilih Tanggal Rencana Kunjungan ']==tanggal]
+        df_new = df[df['Pilih Tanggal Rencana Kunjungan '] ==t anggal]
         df_new
         download = st.button("Download")
